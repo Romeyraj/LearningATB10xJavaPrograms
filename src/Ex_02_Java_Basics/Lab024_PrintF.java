0f1f4f2919020a6c6ced302beb512543944dc286
Romeyraj <iromeyraj.bundela@gmail.com>
2025-02-17 12:16:32 +0530

    17th Feb 2025 - With Print F Assignment

---
+++ src/Ex_02_Java_Basics/Lab024_PrintF.java
@@ -0,0 +1,19 @@
+package Ex_02_Java_Basics;
+
+public class Lab024_PrintF {
+    public static void main(String[] args) {
+        int a =10;
+        System.out.println(a);
+
+        System.out.printf("Your variable name is %d", a);
+        //% d -> int, byte, long, short -data
+        //% s -> Short
+        //%f -> float
+        //%b -> Boolean
+        // when you use a %d, %s, %f or %b as per the value the value takes place in output
+        //Run this command to see
+
+    }
+
+
+        }
---
+++ src/Ex_02_Java_Basics/Printf_Assignment.java
@@ -0,0 +1,27 @@
+package Ex_02_Java_Basics;
+
+public class Printf_Assignment {
+    public static void main(String[] args) {
+        int a = 9;
+        System.out.printf("9*1= %d", a);
+        System.out.printf(" 9*2= %d", a*2);
+        System.out.printf("  9*3= %d", a*3);
+        System.out.printf("   9*4= %d", a*4);
+        System.out.printf("    9*5= %d", a*5);
+        System.out.printf("     9*6= %d", a*6);
+        System.out.printf("      9*7= %d", a*7);
+        System.out.printf("       9*8= %d", a*8);
+        System.out.printf("        9*9= %d", a*9);
+        System.out.printf("         9*10= %d", a*10);
+        //Nine ones are nine
+        //Nine twos are eighteen
+        //Nine threes are twenty-seven
+        //Nine fours are thirty-six
+        //Nine fives are forty-five
+        //Nine sixes are fifty-four
+        //Nine sevens are sixty-three
+        //Nine eights are seventy-two
+        //Nine nines are eighty-one
+        ///Nine tens are ninety
+    }
+}
